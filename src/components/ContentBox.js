a19d7512a8d55ec2af2621368e5a450f81d0caf0
Polina7777 <polina.produho98@mail.ru>
2023-10-19 17:42:41 +0200

    feat:fix cards

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -1,6 +1,8 @@
 'use client';
 import arrowNext from '../assets/image/arrow-next.svg';
 import arrowPrev from '../assets/image/arrow-prev.svg';
+import coin from '../assets/image/coin.svg'
+import hat from '../assets/image/hat.svg'
 // import "node_modules/video-react/dist/video-react.css"; 
 import ReactPlayer from 'react-player/'
 
@@ -18,47 +20,37 @@ export default function defaultContentBox({item}) {
        </div>
        <div className='video-list__wrapper'>
        { item.items.map(item =>{      
-return   <ReactPlayer
-className='react-player'
-url={item.video}
-width='250px'
-height='310px'
-loop
-/>
-//  <div className='player-wrapper'>
-      
-          {/* <video
-    id="my-video"
-    class="video-js"
-    controls
-    preload="auto"
-    width="640"
-    height="264"
-    // poster="MY_VIDEO_POSTER.jpg"
-    data-setup="{}"
+return      <div className='player-wrapper'>   <video
+id="my-video"
+className="video-js"
+// controls
+// preload="auto"
+// width="640"
+// height="264"
+poster={item.poster}
+data-setup="{}"
+>
+<source src={item.video} type="video/mp4" />
+
+{/* <p class="vjs-no-js">
+  To view this video please enable JavaScript, and consider upgrading to a
+  web browser that
+  <a href="https://videojs.com/html5-video-support/" target="_blank"
+    >supports HTML5 video</a
   >
-    <source src={item.video} type="video/mp4" />
-    
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a
-      web browser that
-      <a href="https://videojs.com/html5-video-support/" target="_blank"
-        >supports HTML5 video</a
-      >
-    </p>
-  </video> */}
-  {/* <video     id="my-video"
-    className="video-js"
-    controls
-    // autoPlay
-    preload="auto"
-    width="240"
-    height="300"
-    // poster="MY_VIDEO_POSTER.jpg"
-    data-setup="{}">
-            <source src={item.video} type="video/mp4" />
-    </video> */}
-    //   </div>
+</p> */}
+</video> 
+<p className='video-title'>{item.title}</p>
+<div className='hat_box'>
+  <img src={hat} alt='hat'/>
+</div>
+<div className='coin_box'>
+  <img src={coin} alt='coin' className='coin'/>
+  <p className='coin-title'>Earn 2T</p>
+</div>
+{/* //  <div className='player-wrapper'> */}
+      
+     </div>
 })}
 
      
@@ -66,3 +58,11 @@ loop
     </div>
 )
 }
+{/* <ReactPlayer
+className='react-player'
+url={item.video}
+poster={item.poster}
+// width='250px'
+// height='310px'
+loop
+/> */}
--- src/components/General.js
+++ src/components/General.js
@@ -17,13 +17,19 @@ export default function defaultGeneral() {
         {
         title:'New Playlist',
         // items:[],
-        items:[{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},,{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'}],
+        items:[{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1339/20230726_URXkL69q8t62Ar.jpg'},
+        {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://palmjumeirah.fivehotelsandresorts.com/media/7226/bhwa-launch-web_fb-without-logo-new-min.jpg?anchor=center&mode=crop&width=320&height=450&quality=80'},
+        {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1000/20230613_p03dklJW7DbzJh.jpg'},
+        {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1074/KIwWTYTg-PPEaN3zILdY.jpg'}],
         seeAll:'See All'
         },
         {
             title:'Trending',
             // items:[],
-               items:[{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'},{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment'}],
+               items:[{video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1801/20230510_Pxq69EkIEXHq29.jpg'},
+               {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1764/20171211_163549553.jpg'},
+               {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/2679/20210429_112325437.jpg'},
+               {video:'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',title:'Fundamentals of investment',poster:'https://booker-viva-ie.s3.amazonaws.com/library/1307/20170918_112640638.jpg'}],
             seeAll:'See All'
             }
     ]
